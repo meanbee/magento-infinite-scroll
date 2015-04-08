bc5a96aa693f88530abb75f40c6e85f8e5577364
Nathan McBride <nathan.mcbride@meanbee.com>
2015-04-08 15:30:51 +0100

    Add Pagination Meta Tags to Improve SEO
    Google Webmaster Tool's Guidelines recommend adding rel="next" and
    rel="prev" tags to infinite scroll to help both crawl bots and people
    with accessibility issues crawl your website.

---
+++ app/code/community/Meanbee/InfiniteScroll/Model/Observer.php
@@ -0,0 +1,30 @@
+<?php
+
+class Meanbee_InfiniteScroll_Model_Observer {
+
+    /**
+     * @param Varien_Event_Observer $observer
+     */
+    public function  addPaginationTags(Varien_Event_Observer $observer) {
+        /** @var Meanbee_InfiniteScroll_Helper_Pagination $helper */
+        $helper = Mage::helper('infinitescroll/pagination');
+        /** @var Mage_Core_Controller_Request_Http $request */
+        $request = $observer->getAction()->getRequest();
+        /** @var Mage_Core_Model_Layout $layout */
+        $layout = $observer->getLayout();
+        /** @var Mage_Page_Block_Html_Head $head_block */
+        $head_block = $layout->getBlock('head');
+        if(!$head_block instanceof Mage_Core_Block_Template) {
+            return;
+        }
+        if($helper->isNextUrlAvailable($request)) {
+            $head_block->addLinkRel('next', $helper->getNextPage($request));
+        }
+
+        if($helper->isPreviousUrlAvailable($request)) {
+            $head_block->addLinkRel('prev', $helper->getPreviousPage($request));
+        }
+
+    }
+
+}
